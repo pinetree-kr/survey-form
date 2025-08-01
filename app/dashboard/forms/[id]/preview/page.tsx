f7044e2a25dca74759c4a8d10858c97f1101177e
pinetree-kr <vanadate.kr@gmail.com>
2025-08-01 17:16:33 +0900

    버그 수정

--- app/dashboard/forms/[id]/preview/page.tsx
+++ app/dashboard/forms/[id]/preview/page.tsx
@@ -58,7 +58,7 @@ export default async function SurveyPreviewPage({ params }: SurveyPreviewPagePro
                 상세보기로
               </Link>
               <Link
-                href={`/dashboard/forms/${survey.id}/edit`}
+                href={`/dashboard/forms/${survey.id}`}
                 className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors text-sm"
               >
                 편집하기
